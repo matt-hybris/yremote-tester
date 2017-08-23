5ad48a6404b5a93e0aae5be870159416054d7194
ROSSNER, Matt <matt.rossner@sap.com>
2017-08-23 09:23:16 +0200

    added Javadoc

--- src/main/groovy/com/roznet/ytester/api/TestRunner.groovy
+++ src/main/groovy/com/roznet/ytester/api/TestRunner.groovy
@@ -4,5 +4,10 @@ import com.roznet.ytester.data.TestRunData
 import com.roznet.ytester.data.TestRunResult
 
 interface TestRunner {
+    /**
+     * Runs tests data on a default configured server.
+     * @param testRunData Test suites and test cases to run
+     * @return Test results and errors for failures
+     */
     TestRunResult runTests(TestRunData testRunData)
 }
